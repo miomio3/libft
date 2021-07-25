85277f8a7067368ecb732e042ddda6c548b5732c
緑川実生 <mio@midorikawa-MacBook-Air.local>
2021-07-26 00:59:41 +0900

    strtrim-tochu

--- libft/ft_strtrim.c
+++ libft/ft_strtrim.c
@@ -6,11 +6,41 @@
 /*   By: mio <mio@student.42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/07/08 20:48:36 by mmidorik          #+#    #+#             */
-/*   Updated: 2021/07/26 00:35:39 by mio              ###   ########.fr       */
+/*   Updated: 2021/07/26 00:58:18 by mio              ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
+
+
+char	*ft_substr(char const *s, unsigned int start, size_t len)
+{
+	char	*p;
+	size_t	i;
+	size_t	s_len;
+
+	i = 0;
+	if (s == NULL)
+		return (NULL);
+	while (*(s + start + i))
+		i++;
+	s_len = strlen((char *)s);
+	if (*s == '\0' || start >= s_len)
+		return (strdup(""));
+	if (len < s_len)
+		s_len = len;
+	p = (char *)malloc(sizeof(char) * (s_len + 1));
+	if (p == NULL)
+		return (NULL);
+	i = 0;
+	while (i < len && *(s + start + i))
+	{
+		*(p + i) = *(s + start + i);
+		i++;
+	}
+	*(p + i) = '\0';
+	return (p);
+}
 
 static char	*ft_substr2(char const *front, char const *back)
 {
@@ -27,43 +57,43 @@ static char	*ft_substr2(char const *front, char const *back)
 	return (ft_substr(front, 0, i + 1));
 }
 
-char	*ft_upfind(const char *s, char *c)
+char	*ft_upfind(const char *s, const char *c)
 {
 	size_t	i;
 	size_t	j;
+	char	*sc;
 
-	i = 0;
+	i =0;
+	j = 0;
+	sc = &s[i];
 	while (s[i])
 	{
-		j = 0;
-		while (c[j])
-		{
-			if (s[i] == c[j])
-				break ;
-			j++;
-		}
+		if ((unsigned char)s[i] != (unsigned char)c[j])
+			break ;
 		i++;
+		sc = &s[i];
 	}
+	return (sc);
 }
 
-char	*ft_downfind(const char *s, char *c)
+char	*ft_downfind(const char *s, const char *c)
 {
 	size_t	i;
 	size_t	j;
 
-	i == ft_strlen(s);
-	while (i > 0)
+	i = strlen(s) - 1;//
+	while (i)
 	{
 		j = 0;
 		while (c[j])
 		{
-			if (s[i] == c[j])
+			if ((unsigned char)s[i] != (unsigned char)c[j])
 				break ;
 			j++;
 		}
 		i--;
 	}
-	return (s[i]);
+	return ((char *)&s[i]);
 }
 
 char	*ft_strtrim(char const *s1, char const *set)
@@ -74,8 +104,14 @@ char	*ft_strtrim(char const *s1, char const *set)
 	if (s1 == NULL || set == NULL)
 		return (NULL);
 	tmp = ft_upfind(s1, set);
-	down = ft_downfind(s1 + ft_strlen(s1) - 1, set);
+	down = ft_downfind(s1 + strlen(s1) - 1, set);
 	if (tmp >= down)
-		return (ft_strdup(""));
+		return (strdup(""));//
 	return (ft_substr2(tmp, down));
+}
+
+int main(void)
+{
+	ft_strtrim("   \t  \n\n \t\t  \n\n\nHello \t  Please\n Trim me !\n   \n \n \t\t\n  ",  " \n\t");
+	return (0);
 }
