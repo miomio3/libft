c6489b9a4e71afd36d6e549e7ddd3c35dbf67bb8
miomio3 <uj132139@outlook.jp>
2021-07-26 12:04:02 +0900

    substr

--- libft/ft_substr.c
+++ libft/ft_substr.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_substr.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: mio <mio@student.42.fr>                    +#+  +:+       +#+        */
+/*   By: mmidorik <mmidorik@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/07/08 16:53:10 by mmidorik          #+#    #+#             */
-/*   Updated: 2021/07/26 00:48:33 by mio              ###   ########.fr       */
+/*   Updated: 2021/07/26 12:03:44 by mmidorik         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,11 +21,11 @@ char	*ft_substr(char const *s, unsigned int start, size_t len)
 	i = 0;
 	if (s == NULL)
 		return (NULL);
-	while (*(s + start + i))
-		i++;
 	s_len = ft_strlen((char *)s);
 	if (*s == '\0' || start >= s_len)
 		return (ft_strdup(""));
+	while (*(s + start + i))
+		i++;
 	if (len < s_len)
 		s_len = len;
 	p = (char *)malloc(sizeof(char) * (s_len + 1));
